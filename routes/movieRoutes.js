fd28eaac10935fb38813b5207a06ab3ca88b86f3
Matan Shtar <matanshtar2@gmail.com>
2026-06-17 15:35:51 +0300

    Backend - Setup Express server and TMDB integration structure

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,8 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const { search, getById } = require("../controllers/movieController");
+const { wheel } = require("../controllers/wheelController");
 
 router.get("/search", search);
+// Must come before "/:tmdbId" so "wheel" isn't parsed as a movie id.
+router.get("/wheel", wheel);
 router.get("/:tmdbId", getById);
 
 module.exports = router;
